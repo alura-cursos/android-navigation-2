8d299a2a2d552c854f713148e0248226ec484d1d
Alex Felipe <alexfelipevieira@gmail.com>
2019-06-24 10:26:40 -0300

    projeto inicial

---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -0,0 +1,68 @@
+package br.com.alura.aluraesporte.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
+import br.com.alura.aluraesporte.ui.viewmodel.DetalhesProdutoViewModel
+import kotlinx.android.synthetic.main.detalhes_produto.*
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
+
+class DetalhesProdutoFragment : Fragment() {
+
+    private val argumentos by navArgs<DetalhesProdutoFragmentArgs>()
+    private val produtoId by lazy {
+        argumentos.produtoId
+    }
+    private val viewModel: DetalhesProdutoViewModel by viewModel { parametersOf(produtoId) }
+    private val controlador by lazy { findNavController() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.detalhes_produto,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buscaProduto()
+        configuraBotaoComprar()
+    }
+
+    private fun configuraBotaoComprar() {
+        detalhes_produto_botao_comprar.setOnClickListener {
+            viewModel.produtoEncontrado.value?.let {
+                vaiParaPagamento()
+            }
+        }
+    }
+
+    private fun vaiParaPagamento() {
+        val direcao = DetalhesProdutoFragmentDirections
+            .acaoDetalhesProdutoParaPagamento(produtoId)
+        controlador.navigate(direcao)
+    }
+
+    private fun buscaProduto() {
+        viewModel.produtoEncontrado.observe(this, Observer {
+            it?.let { produto ->
+                detalhes_produto_nome.text = produto.nome
+                detalhes_produto_preco.text = produto.preco.formatParaMoedaBrasileira()
+            }
+        })
+    }
+
+}
---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/PagamentoFragment.kt
@@ -0,0 +1,118 @@
+package br.com.alura.aluraesporte.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
+import br.com.alura.aluraesporte.model.Pagamento
+import br.com.alura.aluraesporte.model.Produto
+import br.com.alura.aluraesporte.ui.viewmodel.PagamentoViewModel
+import kotlinx.android.synthetic.main.pagamento.*
+import org.koin.android.viewmodel.ext.android.viewModel
+
+private const val FALHA_AO_CRIAR_PAGAMENTO = "Falha ao criar pagamento"
+private const val COMPRA_REALIZADA = "Compra realizada"
+
+class PagamentoFragment : Fragment() {
+
+    private val argumentos by navArgs<PagamentoFragmentArgs>()
+    private val produtoId by lazy {
+        argumentos.produtoId
+    }
+    private val viewModel: PagamentoViewModel by viewModel()
+    private lateinit var produtoEscolhido: Produto
+    private val controlador by lazy { findNavController() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.pagamento,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configuraBotaoConfirmaPagamento()
+        buscaProduto()
+    }
+
+    private fun buscaProduto() {
+        viewModel.buscaProdutoPorId(produtoId).observe(this, Observer {
+            it?.let { produtoEncontrado ->
+                produtoEscolhido = produtoEncontrado
+                pagamento_preco.text = produtoEncontrado.preco
+                    .formatParaMoedaBrasileira()
+            }
+        })
+    }
+
+    private fun configuraBotaoConfirmaPagamento() {
+        pagamento_botao_confirma_pagamento.setOnClickListener {
+            criaPagamento()?.let(this::salva) ?: Toast.makeText(
+                context,
+                FALHA_AO_CRIAR_PAGAMENTO,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun salva(pagamento: Pagamento) {
+        if (::produtoEscolhido.isInitialized) {
+            viewModel.salva(pagamento)
+                .observe(this, Observer {
+                    it?.dado?.let {
+                        Toast.makeText(
+                            context,
+                            COMPRA_REALIZADA,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        vaiParaListaProdutos()
+                    }
+                })
+        }
+    }
+
+    private fun vaiParaListaProdutos() {
+        val direcao = PagamentoFragmentDirections.acaoPagamentoParaListaProduto()
+        controlador.navigate(direcao)
+    }
+
+    private fun criaPagamento(): Pagamento? {
+        val numeroCartao = pagamento_numero_cartao
+            .editText?.text.toString()
+        val dataValidade = pagamento_data_validade
+            .editText?.text.toString()
+        val cvc = pagamento_cvc
+            .editText?.text.toString()
+        return geraPagamento(numeroCartao, dataValidade, cvc)
+    }
+
+    private fun geraPagamento(
+        numeroCartao: String,
+        dataValidade: String,
+        cvc: String
+    ): Pagamento? = try {
+        Pagamento(
+            numeroCartao = numeroCartao.toInt(),
+            dataValidade = dataValidade,
+            cvc = cvc.toInt(),
+            produtoId = produtoId,
+            preco = produtoEscolhido.preco
+        )
+    } catch (e: NumberFormatException) {
+        null
+    }
+
+}
