f716e9aee48e6a4c86f5082f82396beadd18b037
Alex Felipe <alexfelipevieira@gmail.com>
2019-07-03 10:19:33 -0300

    3.2

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -1,14 +1,29 @@
 package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import br.com.alura.aluraesporte.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val controlador by lazy {
+        findNavController(R.id.main_activity_nav_host)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        controlador.addOnDestinationChangedListener { controller,
+                                                      destination,
+                                                      arguments ->
+            title = destination.label
+            when(destination.id) {
+                R.id.listaProdutos -> supportActionBar?.show()
+                R.id.login -> supportActionBar?.hide()
+            }
+        }
     }
 
 }
